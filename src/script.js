1e8a7bcbb65c8d26c9bb316a7422bcfed3a95de7
MimTohidi <mim.tohidi@gmail.com>
2021-11-10 23:27:45 +0330

    created script.js

---
+++ src/script.js
@@ -0,0 +1,87 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function showWeather(response) {
+  document.querySelector("#city-name").innerHTML = response.data.name;
+  document.querySelector("#temperature-degree").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+
+  document.querySelector(
+    "#humidity"
+  ).innerHTML = `Humidity : ${response.data.main.humidity}`;
+  document.querySelector("#wind-speed").innerHTML = `Wind : ${Math.round(
+    response.data.wind.speed
+  )} km/h`;
+
+  document.querySelector(
+    "#visibility"
+  ).innerHTML = `visibility : ${response.data.visibility}`;
+
+  document.querySelector("#description-weather").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "066e6846e09ae10d49fa3612a2af48a3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#exampleDataList").value;
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "066e6846e09ae10d49fa3612a2af48a3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let dateElement = document.querySelector("#current-date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-box");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Ilam");
